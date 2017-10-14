71387bf0eeb1804f3e0d94cc3f84933e5ef75bc9
Cathal Killeen <cathalkilleen@gmail.com>
2017-10-14 13:54:30 +0100

    added gmaps key

--- web.js
+++ web.js
@@ -41,7 +41,8 @@ function handleMongoRequest(req, res) {
 
     collection.find({
       Dates: {
-          $gte: new Date(""+requestedYear+"-01-01T00:00:00.000Z")
+          $gte: new Date(""+requestedYear+"-01-01T00:00:00.000Z"),
+          $lt: new Date(""+(requestedYear+1)+"-01-01T00:00:00.000Z")
       }
     }).toArray((err,docs) => {
       console.log(err);
